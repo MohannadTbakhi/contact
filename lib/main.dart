d0afb66e56fb7aa5ea455be6e2948029a259eb9f
Mohannad Tbakhi <mohannadtbakhi80@gmail.com>
2022-07-22 22:28:48 +0300

    second comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,11 +16,63 @@ void main() {
   ));
 }
 
-class ImagePage extends StatelessWidget {
+class ImagePage extends StatefulWidget {
   const ImagePage({Key? key}) : super(key: key);
 
   @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  int leftImageNumber = 1;
+
+  int rightImageNumber = 2;
+
+  void changeImage(){
+    leftImageNumber = Random().nextInt(8) + 1;
+    rightImageNumber = Random().nextInt(8) + 1;
+  };
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          leftImageNumber == rightImageNumber ? "You Won" : "Try Again",
+          style: TextStyle(
+            fontSize: 42,
+            color: Colors.white,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          changeImage();
+                        });
+                      },
+                      child: Image.asset('images/image-$leftImageNumber.png'))),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      changeImage();
+                    });
+                  },
+                  child: Image(
+                    image: AssetImage("images/image-$rightImageNumber.png"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
